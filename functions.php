d7600cb3eae80d31468a4a468ef57abdee2029a1
Nirbhay Singh Rao <nirbhay@live.in>
2022-03-08 13:18:50 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@
 
 function child_enqueue__parent_scripts() {
 
-wp_enqueue_style( ‘parent’, get_template_directory_uri().’/style.css’ );
+wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );
 
 }
 
-add_action( ‘wp_enqueue_scripts’, ‘child_enqueue__parent_scripts’);
+add_action( 'wp_enqueue_scripts', 'child_enqueue__parent_scripts');
